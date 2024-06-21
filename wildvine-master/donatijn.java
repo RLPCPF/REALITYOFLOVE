ea5796869a7140072f8a74773cded9fce5f569a5
RLPCPF <samueloladimeji871@gmail.com>
2024-06-21 13:16:44 +0100

    Add files via upload

---
+++ wildvine-master/donatijn.java
@@ -0,0 +1,10 @@
+<script>
+    // Get the button element by its ID
+    var donationButton = document.getElementById('donationButton');
+
+    // Add a click event listener to the button
+    donationButton.addEventListener('click', function() {
+        // Navigate to another page when the button is clicked
+        window.location.href = 'anotherPage.html';
+    });
+</script>
